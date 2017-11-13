b8ad143d9bc13a2c346897ad7770d0a89333cb22
ljx213101212 <lijixiang.nus@gmail.com>
2017-11-13 23:14:11 +0800

    add PositionReport actor

---
+++ Source/BuildingEscape/PoistionReport.cpp
@@ -0,0 +1,27 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "PoistionReport.h"
+
+
+// Sets default values
+APoistionReport::APoistionReport()
+{
+ 	// Set this actor to call Tick() every frame.  You can turn this off to improve performance if you don't need it.
+	PrimaryActorTick.bCanEverTick = true;
+
+}
+
+// Called when the game starts or when spawned
+void APoistionReport::BeginPlay()
+{
+	Super::BeginPlay();
+	
+}
+
+// Called every frame
+void APoistionReport::Tick(float DeltaTime)
+{
+	Super::Tick(DeltaTime);
+
+}
+
